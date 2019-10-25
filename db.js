f62c9d7356b488a94aaa71d6efd60bdcde2578d0
Eugene Kim <ekim1707@gmail.com>
2019-10-24 21:16:30 -0400

    edit

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 
+let db;
+
 const pgp = require('pg-promise')({
     query: (e) => console.log(e.query)
 })
 
-const db = pgp({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
-
-console.log('just checking');
+if (process.env.DATABASE_URL) {
+    db = pgp(process.env.DATABASE_URL);
+} else {
+    db = pgp({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME
+    });
+}
 
 module.exports = db;
